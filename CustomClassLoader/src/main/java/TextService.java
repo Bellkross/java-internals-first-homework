c94f0bcf4af233f8634a7a97f129466f9ef1539c
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-03-24 23:27:01 +0200

    Add text service class
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ CustomClassLoader/src/main/java/TextService.java
@@ -0,0 +1,15 @@
+public class TextService {
+
+    public String staticText() {
+        return "Some static text";
+    }
+
+    public String variable(String variable) {
+        return variable;
+    }
+
+    public String exception(String text) throws RuntimeException {
+        //TODO throw your custom exception
+        return text;
+    }
+}
