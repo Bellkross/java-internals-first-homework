2b64997fb5da4a7aaf7672491fe58d1c9f56c2c7
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-03-25 21:14:22 +0200

    Modify main class
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- ProxyObject/src/main/java/Main.java
+++ ProxyObject/src/main/java/Main.java
@@ -1,18 +1,14 @@
-import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Proxy;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchMethodException {
-        Object textServiceProxy =
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        TextService textServiceProxy = (TextService)
                 Proxy.newProxyInstance(
                         TextServiceImpl.class.getClassLoader(),
                         TextServiceImpl.class.getInterfaces(),
                         new EnvVariableProxyReplacer(new TextServiceImpl())
                 );
-/*
-        Method variableMethod = TextServiceImpl.class.getMethod("variable", String.class);
-        variableMethod.invoke(textServiceProxy, "server.port=${port}");
-        System.out.println(textServiceProxy.variable());
-*/
+        System.out.println(textServiceProxy.variable("server.port=${port}"));
     }
 }
